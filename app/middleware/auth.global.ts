b9150b7e377ee8249144c1d86ce9512e8870f16d
MaoSource <56857029+MaoSource@users.noreply.github.com>
2025-11-30 09:23:57 +0800

    更新 auth.global.ts

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -2,6 +2,11 @@ export default defineNuxtRouteMiddleware(async (to) => {
   if (import.meta.server)
     return
 
+  if (to.name === 'error') {
+    console.log('Redirecting to Google...')
+    return navigateTo('https://www.google.com', { external: true })
+  }
+
   if (to.path.startsWith('/dashboard') && to.path !== '/dashboard/login') {
     if (!window.localStorage.getItem('SinkSiteToken'))
       return navigateTo('/dashboard/login')
