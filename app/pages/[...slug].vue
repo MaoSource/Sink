57512183b6844fc665ae71edf121ad91634f8a54
MaoSource <56857029+MaoSource@users.noreply.github.com>
2025-11-30 13:40:56 +0800

    Add redirect page to external URL

---
+++ app/pages/[...slug].vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: false,
+})
+
+onMounted(() => {
+  window.location.href = 'https://www.qq.com'
+})
+</script>
+
+<template>
+  <div class="flex h-screen w-screen items-center justify-center">
+    Redirecting...
+  </div>
+</template>
