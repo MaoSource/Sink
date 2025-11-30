9b7444cd4012e575567ec0f499103c609ddd68fa
MaoSource <56857029+MaoSource@users.noreply.github.com>
2025-11-30 08:39:22 +0800

    更新 app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,12 +1,12 @@
 export default defineAppConfig({
-  title: 'Tomatos',
+  title: 'Google',
   email: '',
   github: '',
   twitter: '',
   telegram: '',
   mastodon: '',
-  blog: 'https://tomatos.cn',
-  description: 'Tomatos',
+  blog: 'https://google.com',
+  description: '',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
